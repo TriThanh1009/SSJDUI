b9b31e38047f2ad1553fbd9f5899675871f8db52
Trí Thanh <92020142+TriThanh1009@users.noreply.github.com>
2025-03-21 17:28:15 +0700

    test for mobile

--- src/Pages/ShopComponent/components/HomeComponent/HomeComponent.tsx
+++ src/Pages/ShopComponent/components/HomeComponent/HomeComponent.tsx
@@ -7,27 +7,35 @@ import { getProduct } from '../../../../Responsitories/ProductResponsitory';
 
 const HomeComponent:React.FC = () =>{
   const [products,setproducts] = useState<ProductModel[]>()
+  const [isMobile, setIsMobile] = useState<boolean>(false)
       useEffect(()=>{
         const fetch = async() =>{
           const data = await getProduct()
           setproducts(data)
         }
         fetch()
-      },[])
-  return (
-    <div>
-      <div className="row g-2">
-        { Array.isArray(products) && products.map((product)=>(
-          <div className='col-3' key={product.id}>
-            <ItemCard  products={product}/>
-          </div>
-        ))}
-      </div>
-      <Outlet/>
+        handleResize()
+        window.addEventListener('resize', handleResize)
 
-    </div>
-    
-  )
+        return () => {
+          window.removeEventListener('resize', handleResize)
+        }
+      },[])
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768)
+      }
+      return (
+        <div>
+          <div className="row g-2">
+            {Array.isArray(products) && products.map((product) => (
+              <div className={isMobile ? 'col-12' : 'col-2'} key={product.id}>
+                <ItemCard products={product} />
+              </div>
+            ))}
+          </div>
+          <Outlet />
+        </div>
+      )
 }
 
 
