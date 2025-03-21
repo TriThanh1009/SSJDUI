dc664958e7f4b1b4384b7aef84bde1b3528cdb1e
Trí Thanh <92020142+TriThanh1009@users.noreply.github.com>
2025-03-21 17:44:14 +0700

    bring firebaseconfig to env

---
+++ src/Responsitories/Firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseapikey = import.meta.env.VITE_APP_FIREBASE_API_KEY;
+const firebasedomain = import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN;
+const firebasepjid = import.meta.env.VITE_APP_FIREBASE_PROJECT_ID;
+const firebasepbucket = import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET;
+const firebasesenderid = import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID;
+const firebaseappid = import.meta.env.VITE_APP_FIREBASE_APP_ID;
+const firebasemeasurementid = import.meta.env.VITE_APP_FIREBASE_MEA_ID;
+
+const firebaseConfig = {
+  apiKey: firebaseapikey,
+  authDomain: firebasedomain,
+  projectId: firebasepjid,
+  storageBucket: firebasepbucket,
+  messagingSenderId: firebasesenderid,
+  appId: firebaseappid,
+  measurementId: firebasemeasurementid
+};
+console.log(firebaseapikey)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider }; // Đảm bảo đúng cú pháp export
